c8a3d7c0c04c9625e34e29943955c063f033bc6e
Stre10k-2k <e43810522@gmail.com>
2025-10-09 19:28:20 +0500

    first commit

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 
 LANGUAGE ={
     "English": "en"
-    "RUssiam": "ru"
+    "RUssiam" : "ru"
     "O'zbekcha": "uz"
     "Deutsch": "de"
 }
@@ -32,3 +32,14 @@ def choose_lang(message):
     bot.send_message(message.chat.id, "Выберите язык:", reply_markup=markup)
 
 bot.infinity_polling
+
+
+@bot.message_handler(func=lambda msg:True)
+def translate_or_ask(message):
+    chat_id= message.chat.id
+    text = message.text
+
+    if chat_id in user_langs:
+        target_lang = user_langs[chat_id]
+        try:
+            translator = GoogleTranslator
